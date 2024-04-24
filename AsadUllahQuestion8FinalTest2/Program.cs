39c036458541d30151f64fc9299fcaa6b0b11061
asadu <asadu@MSI>
2024-04-23 21:10:45 -0400

    Add project files.

---
+++ AsadUllahQuestion8FinalTest2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace AsadUllahQuestion8FinalTest2
+{
+    public class CalculationEventArgs : EventArgs
+    {
+        public int Result { get; private set; }
+
+        public CalculationEventArgs(int result)
+        {
+            Result = result;
+        }
+    }
+
+    public delegate void CalculationPerformedHandler(object sender, CalculationEventArgs e);
+
+    public class Calculator
+    {
+        public event CalculationPerformedHandler CalculationPerformed;
+
+        public void Add(int a, int b)
+        {
+            int result = a + b;
+            OnCalculationPerformed(new CalculationEventArgs(result));
+        }
+
+        protected virtual void OnCalculationPerformed(CalculationEventArgs e)
+        {
+            CalculationPerformed?.Invoke(this, e);
+        }
+    }
+
+    public class CalculationHandler
+    {
+        public CalculationHandler(Calculator calculator)
+        {
+
+            calculator.CalculationPerformed += OnCalculationPerformed;
+        }
+
+
+        private void OnCalculationPerformed(object sender, CalculationEventArgs e)
+        {
+            Console.WriteLine($"Calculation result: {e.Result}");
+        }
+    }
+
+
+    static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    
+}
